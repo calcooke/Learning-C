1dc134b83fdd60efe438a9d03eeec638d7227721
ziggyzulu <k00083370@student.lit.ie>
2020-04-03 11:00:36 +0100

    text placeholder

--- dummy2.c
+++ dummy2.c
@@ -8,7 +8,9 @@ int main(){
 
     printf("Hello \n world");
 
-    printf("Everyone knows that %d + %d = %d", 2, 2, 2);
+    printf("Everyone knows that %d + %d = %d \n", 2, 2, 2 + 2);
+
+    printf("Good %s \n", "morning");
 
     return(0);
 
