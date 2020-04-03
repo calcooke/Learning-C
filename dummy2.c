5a54f461cb9a2ab6b44cde2d0ff2fe20718c8123
ziggyzulu <k00083370@student.lit.ie>
2020-04-03 17:20:04 +0100

    Declare and assign variables

--- dummy2.c
+++ dummy2.c
@@ -4,15 +4,21 @@
 
 int main(){
 
-    puts("This is great");
+    // puts("This is great");
 
-    printf("Hello \n world");
+    // printf("Hello \n world");
 
-    printf("Everyone knows that %d + %d = %d \n", 2, 2, 2 + 2);
+    // printf("Everyone knows that %d + %d = %d \n", 2, 2, 2 + 2);
 
-    printf("Good %s \n", "morning");
+    // printf("Good %s \n", "morning");
 
-    printf("You are a %c %s \n", 'C', "programmer");
+    // printf("You are a %c %s \n", 'C', "programmer");
+
+    int age;
+
+    age = 30;
+
+    printf("Current age is %d \n", age);
 
     return(0);
 
